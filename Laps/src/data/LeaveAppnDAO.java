79caadd3716cd11ee533669fd8a6e6590e60149c
melvchew <melvchew@gmail.com>
2017-06-11 18:31:48 +0800

    Changed LeaveAppnDAO and LeaveAppnDAOImpl

--- Laps/src/data/LeaveAppnDAO.java
+++ Laps/src/data/LeaveAppnDAO.java
@@ -1,6 +1,5 @@
 package data;
 
-import java.sql.Date;
 import java.util.ArrayList;
 
 import model.LeaveAppnDTO;
@@ -11,7 +10,7 @@ public interface LeaveAppnDAO {
 	public void insertLeaveAppn(LeaveAppnDTO dto) throws Exception;
 	public void updateLeaveAppn(LeaveAppnDTO dto) throws Exception; //reject and accept leaveappn
 	public void deleteLeaveAppn(LeaveAppnDTO dto) throws Exception; //change status column to deleted.
-	public ArrayList<LeaveAppnDTO> getAllLeaveAppn(LeaveAppnDTO dto) throws Exception;
+	public ArrayList<LeaveAppnDTO> getAllLeaveAppn() throws Exception;
 	public ArrayList<LeaveAppnDTO> getLeaveAppn(UsersDTO user) throws Exception;
 	public ArrayList<LeaveAppnDTO> getPendingLeaveAppn() throws Exception;
 }
--- Laps/src/data/LeaveAppnDAOImpl.java
+++ Laps/src/data/LeaveAppnDAOImpl.java
@@ -1,5 +1,93 @@
 package data;
 
-public class LeaveAppnDAOImpl {
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.LeaveAppnDTO;
+import model.UsersDTO;
+
+public class LeaveAppnDAOImpl {
+	
+	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("jpa-laps");
+	EntityManager entitymanager = emfactory.createEntityManager();
+	
+	public void insertLeaveAppn(LeaveAppnDTO dto) throws Exception {
+		try {
+
+			 entitymanager.getTransaction().begin();
+			  entitymanager.persist(dto);
+			  entitymanager.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateLeaveAppn(LeaveAppnDTO dto) throws Exception {
+		try {
+			LeaveAppnDTO user = entitymanager.find(LeaveAppnDTO.class, dto.getAppnId());
+			entitymanager.getTransaction().begin();
+			user.setStatus(dto.getStatus());
+			entitymanager.getTransaction().commit();
+
+		} catch (Exception e) {
+			String msg = "Error when Updating Leave Application. Message: " + e;
+			throw new DAOException(msg);
+		}
+	} //reject and accept leaveappn
+	public void deleteLeaveAppn(LeaveAppnDTO dto) throws Exception {
+		try {
+			LeaveAppnDTO user = entitymanager.find(LeaveAppnDTO.class, dto.getAppnId());
+			entitymanager.getTransaction().begin();
+			user.setStatus("DELETED");
+			entitymanager.getTransaction().commit();
+
+		} catch (Exception e) {
+			String msg = "Error when Deleting Leave Application. Message: " + e;
+			throw new DAOException(msg);
+		}
+	} //change status column to deleted.
+	public ArrayList<LeaveAppnDTO> getAllLeaveAppn() throws Exception {
+		List<LeaveAppnDTO> laplist = new ArrayList<>();
+		try {
+			laplist = entitymanager.createQuery("SELECT u FROM LeaveAppnDTO u", LeaveAppnDTO.class).getResultList();
+					
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+		return laplist.size() == 0? null : new ArrayList<LeaveAppnDTO>(laplist);
+	}
+	
+	
+	public ArrayList<LeaveAppnDTO> getLeaveAppn(UsersDTO user) throws Exception {
+		List<LeaveAppnDTO> laplist = new ArrayList<>();
+		try {
+			laplist = entitymanager.createQuery("SELECT u FROM LeaveAppnDTO u WHERE u.userId = :uname", LeaveAppnDTO.class).setParameter("uname", user.getUserId()).getResultList();
+					
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+		return laplist.size() == 0? null : new ArrayList<LeaveAppnDTO>(laplist);
+	
+	}
+	
+	
+	public ArrayList<LeaveAppnDTO> getPendingLeaveAppn() throws Exception {
+		List<LeaveAppnDTO> laplist = new ArrayList<>();
+		try {
+			laplist = entitymanager.createQuery("SELECT u FROM LeaveAppnDTO u where u.status = 'PENDING'", LeaveAppnDTO.class).getResultList();
+					
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+		return laplist.size() == 0? null : new ArrayList<LeaveAppnDTO>(laplist);
+	
+	}
 }
