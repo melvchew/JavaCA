a7ed1bf294854b2d0f713429395f695d7b4967f0
ZweWaiYanHtet <zwyhmdy@gmail.com>
2017-06-11 14:55:21 +0800

    UsersDAO Impl added

--- Laps/src/data/UsersDAO.java
+++ Laps/src/data/UsersDAO.java
@@ -6,10 +6,10 @@ import model.UsersDTO;
 
 public interface UsersDAO {
 
-	public void insertUser(UsersDTO DTO) throws Exception;
-	public void updateUser(UsersDTO DTO) throws Exception;
-	public UsersDTO getUser(int userid) throws Exception;
-	public ArrayList<UsersDTO> getUsersByManager(UsersDTO user) throws Exception;
-	public void deleteUser(UsersDTO DTO) throws Exception;
-	public boolean checkUser(String username, String password) throws Exception;
+	public void insertUser(UsersDTO DTO) throws DAOException;
+	public void updateUser(UsersDTO DTO) throws DAOException;
+	public UsersDTO getUser(String username) throws DAOException;
+	public ArrayList<UsersDTO> getUsersByManager(UsersDTO user) throws DAOException;
+	public void deleteUser(UsersDTO DTO) throws DAOException;
+	public boolean checkUser(String username, String password) throws DAOException;
 }
--- Laps/src/data/UsersDAOImpl.java
+++ Laps/src/data/UsersDAOImpl.java
@@ -1,47 +1,111 @@
 package data;
 
-import java.util.ArrayList;
+import java.util.*;
+
 import model.UsersDTO;
 import javax.persistence.*;
+import model.UsersDTO;
 
 public class UsersDAOImpl implements UsersDAO {
-	
-	
+
+	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("jpa-laps");
+	EntityManager entitymanager = emfactory.createEntityManager();
 
 	@Override
-	public void insertUser(UsersDTO DTO) throws Exception {
-		// TODO Auto-generated method stub
+	public void insertUser(UsersDTO DTO) throws DAOException {
+		try {
+			UsersDTO user = DTO;
+
+			entitymanager.getTransaction().begin();
+			entitymanager.persist(DTO);
+			entitymanager.getTransaction().commit();
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+	}
+
+	@Override
+	public void updateUser(UsersDTO DTO) throws DAOException {
+		try {
+			UsersDTO user = entitymanager.find(UsersDTO.class, DTO.getUserId());
+
+			entitymanager.getTransaction().begin();
+			user.setUsername(DTO.getUsername());
+			user.setPassword(DTO.getPassword());
+			user.setPositionId(DTO.getPositionId());
+			user.setName(DTO.getName());
+			user.setDob(DTO.getDob());
+			user.setPhone(DTO.getPhone());
+			user.setAddress(DTO.getAddress());
+			user.setEmail(DTO.getEmail());
+			user.setManagerId(DTO.getManagerId());
+			user.setOtHours(DTO.getOtHours());
+			entitymanager.getTransaction().commit();
+
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+
+	}
+
+	@Override
+	public UsersDTO getUser(String username) throws DAOException {
+		UsersDTO user = new UsersDTO();
+
+		try {
+			user = entitymanager.createQuery("SELECT u FROM TABLE users WHERE u.username = :uname", UsersDTO.class)
+					.setParameter("uname", username).getSingleResult();
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+		return user;
+	}
+
+	@Override
+	public ArrayList<UsersDTO> getUsersByManager(UsersDTO user) throws DAOException {
+		List<UsersDTO> users = new ArrayList<>();
+		try {
+			users = entitymanager.createQuery("SELECT u FROM TABLE users WHERE u.manager_id = :mname", UsersDTO.class)
+					.setParameter("uname", user.getUserId()).getResultList();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return new ArrayList<UsersDTO>(users);
+	}
+
+	@Override
+	public void deleteUser(UsersDTO DTO) throws DAOException {
+		try {
+			UsersDTO user = entitymanager.find(UsersDTO.class, DTO.getUserId());
+
+			entitymanager.getTransaction().begin();
+			// change delete flag column to 1
+			entitymanager.getTransaction().commit();
+
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
+
+	}
+
+	@Override
+	public boolean checkUser(String username, String password) throws DAOException {
+		UsersDTO user = new UsersDTO();
+
+		try {
+			user = entitymanager.createQuery("SELECT u FROM TABLE users WHERE u.username = :uname AND u.password = :password", UsersDTO.class)
+					.setParameter("uname", username).setParameter("password", password).getSingleResult();
+		} catch (Exception e) {
+			String msg = "Error when inserting user. Message: " + e;
+			throw new DAOException(msg);
+		}
 		
-	}
-
-	@Override
-	public void updateUser(UsersDTO DTO) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public UsersDTO getUser(int userid) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<UsersDTO> getUsersByManager(UsersDTO user) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteUser(UsersDTO DTO) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean checkUser(String username, String password) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		boolean result = user.getUsername().equals(null)? false : true;
+		return result;
 	}
 
 }
